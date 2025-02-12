48ebf7c3c9c56205ba95ecf8ab72741073f82996
MaksFULL <169079873+MaksFULL@users.noreply.github.com>
2025-02-12 10:14:32 +0200

    sdsdd

--- src/basic/3.ts
+++ src/basic/3.ts
@@ -2,10 +2,19 @@ let stringOrNumber: string | number;
 let enabledOrDisabled: "enable" | "disable";
 
 console.log("---------------------3 task------------------");
-console.log((stringOrNumber = "hello"));
-console.log((stringOrNumber = 100));
-// console.log(stringOrNumber = {}); //error
 
-console.log((enabledOrDisabled = "disable"));
-console.log((enabledOrDisabled = "enable"));
-// console.log(enabledOrDisabled = "hello") // error
+stringOrNumber = "hello";
+console.log(stringOrNumber); // Теперь переменная читается
+
+stringOrNumber = 100;
+console.log(stringOrNumber); // Теперь переменная читается
+
+// console.log(stringOrNumber = {}); // Ошибка, т.к. {} не соответствует string | number
+
+enabledOrDisabled = "disable";
+console.log(enabledOrDisabled); // Теперь переменная читается
+
+enabledOrDisabled = "enable";
+console.log(enabledOrDisabled); // Теперь переменная читается
+
+// console.log(enabledOrDisabled = "hello"); // Ошибка, т.к. "hello" не соответствует "enable" | "disable"
